a45b6f9574f9f1ccfe2509fabc0022ba8b29e27e
zhy <zhouhaoyang618@gmail.com>
2026-01-23 17:44:43 +0800

    添加了开始游戏动画和背景音乐以及玩家成功失败得分的音效文件

---
+++ new/NewProject2d/assets/code/bgm.ts
@@ -0,0 +1,23 @@
+import { _decorator, Component, director } from 'cc';
+const { ccclass } = _decorator;
+
+@ccclass('bgm')
+export class bgm extends Component {
+    private static _instance: bgm | null = null;
+
+    onLoad() {
+        if (bgm._instance && bgm._instance !== this) {
+            this.node.destroy();
+            return;
+        }
+
+        bgm._instance = this;
+
+        if (this.node.parent) {
+            this.node.setParent(null);
+        }
+
+        director.addPersistRootNode(this.node);
+    }
+}
+
